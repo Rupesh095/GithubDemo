0df10577f8dfd6c174e623253c6c54408b899893
Rupesh Sahu <rupeshksahu7@gmail.com>
2021-01-01 10:36:55 +0530

    Added First Comment

--- app/src/main/java/com/app/githubdemo/ThirdActivity.java
+++ app/src/main/java/com/app/githubdemo/ThirdActivity.java
@@ -10,5 +10,8 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        /*-- Comment First --*/
+
     }
 }
